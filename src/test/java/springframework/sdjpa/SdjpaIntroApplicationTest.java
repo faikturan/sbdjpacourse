c0c662f19b30a3b533deadf703e8376b8c70ccf3
FAİK TURAN <faik.turan@secomtr.com>
2023-03-21 11:23:12 +0100

    Section-6

--- src/test/java/springframework/sdjpa/SdjpaIntroApplicationTest.java
+++ src/test/java/springframework/sdjpa/SdjpaIntroApplicationTest.java
@@ -1,12 +1,14 @@
 package springframework.sdjpa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import springframework.sdjpa.repositories.BookRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SdjpaIntroApplicationTest {
 
+    @Autowired
     BookRepository bookRepository;
 
 
